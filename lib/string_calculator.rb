a80b5922760f9984114f58d6f9bf56f8bb5937f6
Mahesh Uppuluri <uppuluri.mahesh@gmail.com>
2016-10-21 10:39:54 +0100

    1 number passing

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,15 @@
 class StringCalculator
+  def add string_numbers
+      0 if string_numbers == ""
+      string_to_integer string_numbers
+  end
+
+private
+
+  def string_to_integer string
+    string.to_i
+  end
+
+
 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,6 +6,12 @@ describe StringCalculator do
     it "takes an empty string and returns 0" do
       expect(string_calculator.add("")).to eq(0)
     end
+    it "takes 1 number" do
+      expect(string_calculator.add("1")).to eq(1)
+    end
+
+    
+
   end
 
 end
