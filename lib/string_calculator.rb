8771950927f8804050e35364ee010a75f781444e
Mahesh Uppuluri <uppuluri.mahesh@gmail.com>
2016-10-21 17:55:54 +0100

    negative number now passing

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,8 +4,9 @@ class StringCalculator
   def add string_numbers
     numbers = string_numbers.split(/[\n,]/)
     raise "negatives not allowed" if numbers.any?{ |string| string.include?('-') }
-    p numbers
-    numbers[0] == nil ? 0 : numbers.map! {|number| number.to_i}.reduce(:+)
+    digits = numbers.map! {|number| number.to_i}
+    digits.collect!{|digit| digit >= 1000 ? digit = 0 : digit = digit }
+    numbers[0] == nil ? 0 : digits.reduce(:+)
   end
 
 
