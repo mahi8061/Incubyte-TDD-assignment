2c186e5634c5ed0c1312fb529b31a2573a526bb1
Mahesh Uppuluri <uppuluri.mahesh@gmail.com>
2016-10-21 16:10:05 +0100

    test for negatives done

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,7 @@ class StringCalculator
 
   def add string_numbers
     numbers = string_numbers.split(/[\n,]/)
-    p numbers
+    numbers.contain("-")
     numbers[0] == nil ? 0 : numbers.map! {|number| number.to_i}.reduce(:+)
   end
 
