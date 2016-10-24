f96975ba3c03b8f8b2d517e085a72179f76b774f
Mahesh Uppuluri <uppuluri.mahesh@gmail.com>
2016-10-24 11:32:03 +0100

    refactored interface with private methods

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,15 +1,30 @@
 class StringCalculator
 
+  NEGATIVES = "negatives not allowed"
 
   def add string_numbers
-    numbers = string_numbers.split(/[\n,]/)
-    raise "negatives not allowed" if numbers.any?{ |string| string.include?('-') }
-    digits = numbers.map! {|number| number.to_i}
-    digits.collect!{|digit| digit >= 1000 ? digit = 0 : digit = digit }
+    numbers = split_the_string string_numbers
+    raise NEGATIVES if negatives? numbers
+    digits = convert_to_integers numbers
+    remove_numbers_above_1000 digits
     numbers[0] == nil ? 0 : digits.reduce(:+)
   end
 
+private
 
+  def split_the_string string
+    string.split(/[\n,]/)
+  end
 
+  def negatives? numbers
+    numbers.any?{ |string| string.include?('-') }
+  end
 
+  def convert_to_integers numbers
+    numbers.map! {|number| number.to_i}
+  end
+
+  def remove_numbers_above_1000 digits
+    digits.collect!{|digit| digit >= 1000 ? digit = 0 : digit = digit }
+  end
 end
