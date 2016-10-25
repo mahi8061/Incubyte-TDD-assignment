4aaf6597b81b94607c35e64ed76a22cfbfbf21d9
Mahesh Uppuluri <uppuluri.mahesh@gmail.com>
2016-10-25 16:45:58 +0100

    refactored

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,7 @@
 class StringCalculator
-
-  NEGATIVES = "negatives not allowed"
+  NEGATIVES = 'negatives not allowed'.freeze
   ZERO = 0
-  def add string_numbers
+  def add(string_numbers)
     numbers = split_the_string string_numbers
     if delimiters? numbers
       delimiter = delimiter(numbers)
@@ -16,37 +15,35 @@ class StringCalculator
 
 private
 
-  def split_the_string string
+  def split_the_string(string)
     string.split(/[\n,]/)
   end
 
-  def negatives? numbers
-    numbers.any?{ |string| string.include?('-') }
+  def negatives?(numbers)
+    numbers.any? { |string| string.include?('-') }
   end
 
-  def convert_to_integers numbers
-    numbers.map! {|number| number.to_i}
+  def convert_to_integers(numbers)
+    numbers.map!(&:to_i)
   end
 
-  def remove_numbers_above_1000 digits
-    digits.collect!{|digit| digit >= 1000 ? digit = 0 : digit = digit }
+  def remove_numbers_above_1000(digits)
+    digits.collect! { |digit| digit >= 1000 ? digit = 0 : digit = digit }
   end
 
-  def delimiters? numbers
-    numbers.any?{|number| number.include?("//")}
+  def delimiters?(numbers)
+    numbers.any? { |number| number.include?('//') }
   end
 
-  def delimiter numbers
+  def delimiter(numbers)
     numbers.first.delete('//[]')
   end
 
-  def remove_delimiter numbers, delimiter
-    numbers.last.delete(delimiter).split("")
+  def remove_delimiter(numbers, delimiter)
+    numbers.last.delete(delimiter).split('')
   end
 
-  def zero? numbers
-    numbers.first == nil
+  def zero?(numbers)
+    numbers.first.nil?
   end
-
-
 end
