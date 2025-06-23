ceca2cbdab4ae38812bcaeef0c94df9d481b7220
Mahesh Uppuluri <mahesh.u@agribazaar.com>
2025-06-23 17:22:52 +0530

    first input

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,19 @@
+require "string_calculator"
+describe StringCalculator do
+  subject(:string_calculator) {described_class.new}
+
+  describe "#add" do
+    it "takes an empty string and returns 0" do
+      expect(string_calculator.add("")).to eq(0)
+    end
+    it "takes 1 number" do
+      expect(string_calculator.add("1")).to eq(1)
+    end
+
+    it "takes 2 numbers" do
+      expect(string_calculator.add("1,2")).to eq(3)
+    end
+
+  end
+
+end
