cb533bdfda6904777cd7ed14fa926e87ac36e7d0
Mahesh Uppuluri <mahesh.u@agribazaar.com>
2025-06-23 17:26:18 +0530

    refactor

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def add string_numbers
-    numbers = string_numbers.split(/[\n,]/);
+    numbers = string_numbers.split(/[\n,]/)
+    p numbers
     numbers[0] == nil ? 0 : numbers.map! {|number| number.to_i}.reduce(:+)
   end
 end
