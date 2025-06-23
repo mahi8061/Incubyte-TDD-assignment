75dc8afbc128544d54fdf235a1ad665197f2c588
Mahesh Uppuluri <mahesh.u@agribazaar.com>
2025-06-23 17:24:00 +0530

    multiple input case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,6 +14,13 @@ describe StringCalculator do
       expect(string_calculator.add("1,2")).to eq(3)
     end
 
+    it "handles an unknown amount of numbers" do
+      expect(string_calculator.add("1,2,3,4,5")).to eq(15)
+    end
+
+    it "handles new lines e.g 1\n2,3" do
+      expect(string_calculator.add("1\n2,3")).to eq(6)
+    end
   end
 
 end
